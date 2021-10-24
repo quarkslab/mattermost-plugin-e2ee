d76c8ae670bdf272d84693dfdc254332f9044574
Adrien Guinet <adrien@guinet.me>
2021-10-24 14:04:40 +0200

    Show a message when a new recipient is used for encryption
    This can detect a rogue server that adds some form of "ghost user" to a
    channel to get the messages sent to that channel.
    
    Closes #11

--- webapp/tests/pubkeys_storage.test.ts
+++ webapp/tests/pubkeys_storage.test.ts
@@ -1,7 +1,7 @@
 import 'mattermost-webapp/tests/setup';
 import {jest} from '@jest/globals';
 
-import {pubkeyStore} from '../src/pubkeys_storage';
+import {pubkeyStore, getNewChannelPubkeys, storeChannelPubkeys} from '../src/pubkeys_storage';
 import {PrivateKeyMaterial} from '../src/e2ee';
 
 test('pubkeysStore', async () => {
@@ -19,4 +19,26 @@ test('pubkeysStore', async () => {
 
     hasChanged = await pubkeyStore('user1', pubkey2);
     expect(hasChanged).toStrictEqual(true);
+});
+
+test('channelPubkeys', async () => {
+    const privkey0 = await PrivateKeyMaterial.create(false /* exportable */);
+    const pubkey0 = await privkey0.pubKey();
+    const uid0 = 'user0';
+    const privkey1 = await PrivateKeyMaterial.create(false /* exportable */);
+    const pubkey1 = await privkey1.pubKey();
+    const uid1 = 'user1';
+
+    const chanID = 'mychan';
+    const pubkeys = new Map([[uid0, pubkey0]]);
+    let ret = await getNewChannelPubkeys(chanID, pubkeys);
+    expect(ret).toStrictEqual(Array.from(pubkeys));
+    await storeChannelPubkeys(chanID, Array.from(pubkeys.values()));
+
+    ret = await getNewChannelPubkeys(chanID, pubkeys);
+    expect(ret).toStrictEqual([]);
+
+    pubkeys.set(uid1, pubkey1);
+    ret = await getNewChannelPubkeys(chanID, pubkeys);
+    expect(ret).toStrictEqual([[uid1, pubkey1]]);
 });
